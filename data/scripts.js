c91e38512595670014208c8723200f2257e1c01f
awolffromspace <ewolft@gmail.com>
2015-05-08 21:10:11 -0400

    Update scripts.js

--- data/scripts.js
+++ data/scripts.js
@@ -4387,7 +4387,7 @@ exports.BattleScripts = {
 			var tier = template.tier;
 			switch (tier) {
 			case 'LC':
-				if (puCount > 1 && this.random(5) >= 1) continue;
+				if (puCount > 1) continue;
 				break;
 			case 'PU':
 				// PUs are limited to 2 but have a 20% chance of being added anyway.
@@ -4434,16 +4434,7 @@ exports.BattleScripts = {
 				if (template.species !== 'Pikachu' && this.random(30) >= 1) continue;
 			}
 
-			// Limit 2 of any type
 			var types = template.types;
-			var skip = false;
-			for (var t = 0; t < types.length; t++) {
-				if (typeCount[types[t]] > 1 && this.random(5) >= 1) {
-					skip = true;
-					break;
-				}
-			}
-			if (skip) continue;
 
 			if (potd && potd.exists) {
 				// The Pokemon of the Day belongs in slot 2
@@ -4463,14 +4454,6 @@ exports.BattleScripts = {
 
 			// Illusion shouldn't be on the last pokemon of the team
 			if (set.ability === 'Illusion' && pokemonLeft > 4) continue;
-
-			// Limit 1 of any type combination
-			var typeCombo = types.join();
-			if (set.ability === 'Drought' || set.ability === 'Drizzle') {
-				// Drought and Drizzle don't count towards the type combo limit
-				typeCombo = set.ability;
-			}
-			if (typeCombo in typeComboCount) continue;
 
 			// Limit the number of Megas to one
 			var forme = template.otherFormes && this.getTemplate(template.otherFormes[0]);
