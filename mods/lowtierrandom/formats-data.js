ff534219e735070dc1b40325a7ad4416645c46bb
awolffromspace <ewolft@gmail.com>
2015-05-14 16:52:43 -0400

    Update Low Tier Random Battle

--- mods/lowtierrandom/formats-data.js
+++ mods/lowtierrandom/formats-data.js
@@ -2,12 +2,31 @@ exports.BattleFormatsData = {
 	kangaskhanmega: {
 		randomBattleMoves: ["fakeout","return","suckerpunch","earthquake","poweruppunch","crunch"],
 		randomDoubleBattleMoves: ["fakeout","return","suckerpunch","earthquake","doubleedge","poweruppunch","drainpunch","crunch","protect"],
-
 		tier: "Uber"
+	},
+	pinsirmega: {
+		randomBattleMoves: ["swordsdance","earthquake","closecombat","quickattack","return"],
+		randomDoubleBattleMoves: ["feint","protect","swordsdance","xscissor","earthquake","closecombat","substitute","quickattack","return","rockslide"],
+		tier: "BL"
+	},
+	houndoommega: {
+		randomBattleMoves: ["nastyplot","darkpulse","taunt","fireblast","hiddenpowergrass"],
+		randomDoubleBattleMoves: ["nastyplot","darkpulse","taunt","heatwave","hiddenpowergrass","protect"],
+		tier: "BL2"
+	},
+	gallademega: {
+		randomBattleMoves: ["swordsdance","closecombat","drainpunch","knockoff","zenheadbutt","substitute"],
+		randomDoubleBattleMoves: ["closecombat","stoneedge","drainpunch","icepunch","zenheadbutt","swordsdance","knockoff","protect"],
+		tier: "BL"
 	},
 	mawilemega: {
 		randomBattleMoves: ["swordsdance","ironhead","firefang","substitute","playrough","suckerpunch","knockoff","focuspunch"],
 		randomDoubleBattleMoves: ["swordsdance","ironhead","firefang","substitute","playrough","suckerpunch","knockoff","protect"],
 		tier: "Uber"
+	},
+	medichammega: {
+		randomBattleMoves: ["highjumpkick","drainpunch","icepunch","fakeout","zenheadbutt"],
+		randomDoubleBattleMoves: ["highjumpkick","drainpunch","zenheadbutt","icepunch","bulletpunch","protect","fakeout"],
+		tier: "BL"
 	}
 };
