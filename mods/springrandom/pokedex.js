c5dcd509cfe7469c580573e5ad615f5df9146e99
awolffromspace <ewolft@gmail.com>
2015-05-14 13:49:45 -0400

    Update pokedex.js

--- mods/springrandom/pokedex.js
+++ mods/springrandom/pokedex.js
@@ -2,5 +2,5 @@ exports.BattlePokedex = {
 castform:{num:351,species:"Castform",types:["Normal"],baseStats:{hp:85,atk:85,def:85,spa:85,spd:85,spe:85},abilities:{0:"Forecast"},heightm:0.3,weightkg:0.8,color:"White",eggGroups:["Fairy","Amorphous"],otherFormes:["castformsunny","castformrainy","castformsnowy"]},
 castformsunny:{num:351,species:"Castform-Sunny",baseSpecies:"Castform",forme:"Sunny",formeLetter:"S",types:["Fire"],baseStats:{hp:85,atk:85,def:85,spa:85,spd:85,spe:85},abilities:{0:"Forecast"},heightm:0.3,weightkg:0.8,color:"White",eggGroups:["Fairy","Amorphous"]},
 castformrainy:{num:351,species:"Castform-Rainy",baseSpecies:"Castform",forme:"Rainy",formeLetter:"R",types:["Water"],baseStats:{hp:85,atk:85,def:85,spa:85,spd:85,spe:85},abilities:{0:"Forecast"},heightm:0.3,weightkg:0.8,color:"White",eggGroups:["Fairy","Amorphous"]},
-castformsnowy:{num:351,species:"Castform-Snowy",baseSpecies:"Castform",forme:"Snowy",formeLetter:"S",types:["Ice"],baseStats:{hp:85,atk:85,def:85,spa:85,spd:85,spe:85},abilities:{0:"Forecast"},heightm:0.3,weightkg:0.8,color:"White",eggGroups:["Fairy","Amorphous"]},
+castformsnowy:{num:351,species:"Castform-Snowy",baseSpecies:"Castform",forme:"Snowy",formeLetter:"S",types:["Ice"],baseStats:{hp:85,atk:85,def:85,spa:85,spd:85,spe:85},abilities:{0:"Forecast"},heightm:0.3,weightkg:0.8,color:"White",eggGroups:["Fairy","Amorphous"]}
 };
